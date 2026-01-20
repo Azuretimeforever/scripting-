f6425514ad74fbd804af4e9a1540e9aeea4e58f9
Azuretimeforever <sofiavladbibi@gmail.com>
2026-01-20 16:56:40 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Este codigo es para que diga Hello, World! (Primera vez usandolo :) )
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
