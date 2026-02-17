19e15e9cacc31510114151bcdcebb51e8c3dc694
Azuretimeforever <sofiavladbibi@gmail.com>
2026-02-17 17:09:29 +0100

    Add files via upload

---
+++ Transport.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+    public Transform teleportPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = teleportPoint.position;
+    }
+}
